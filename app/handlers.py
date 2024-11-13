f58196f72a90dfb47add30cdafddf59830f8b08b
All Mute ✓ <davidmerkulov@yandex.ru>
2024-11-13 23:28:27 +0300

    init

---
+++ app/handlers.py
@@ -0,0 +1,74 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from loguru import logger
+import dotenv, os
+from telegram.error import BadRequest
+
+dotenv.load_dotenv()
+
+DB_CHOICE = os.getenv("DB_CHOICE") == "pocketbase"
+if DB_CHOICE:
+    from app.pb import init_commands_and_snippets, get_all_chats, create_group_chat_id, remove_group_chat_id, get_snippet_by_name
+else:
+    from app.fakepb import init_commands_and_snippets, get_all_chats, create_group_chat_id, remove_group_chat_id, get_snippet_by_name
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    from app.bot import commands
+    logger.debug(f"Вызвана команда /start от пользователя с ID: {update.effective_user.id}")
+    await update.message.reply_text(
+        commands.get("start", "default start message"),
+    )
+    
+async def add_chat_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    from app.bot import commands
+    logger.debug(f"Добавление чата с ID: {update.effective_chat.id}")
+    create_group_chat_id(update.effective_chat.id)
+    await update.message.reply_text(
+        commands.get("add_chat", "default add chat message"),
+    )
+    
+async def remove_chat_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    from app.bot import commands
+    logger.debug(f"Удаление чата с ID: {update.effective_chat.id}")
+    remove_group_chat_id(update.effective_chat.id)
+    await update.message.reply_text(
+        commands.get("remove_chat", "default remove chat message"),
+    )
+    
+async def snippet_response_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.debug("Обработка сниппета")
+    snippet_text = get_snippet_by_name(update.message.text.split()[0][1:])
+    await update.message.reply_text(
+        snippet_text,
+    )
+
+async def all_guides_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    from app.bot import commands
+    logger.debug("Запрос всех руководств")
+    __commands, snippets = init_commands_and_snippets()
+    await update.message.reply_text(
+        commands.get("all_guides", "default all guides message") + "\n" + "\n".join([f"/{key}" for key, value in snippets.items()]),
+    )
+    
+async def send_news_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.debug("Отправка новостей во все чаты")
+    # Проверяем, есть ли текст сообщения
+    if len(update.message.text.split(" ", 1)) < 2:
+        await update.message.reply_text("Ошибка: сообщение пустое. Пожалуйста, введите текст новости.")
+        return
+    
+    message = update.message.text.split(" ", 1)[1]
+    chat_ids = get_all_chats()
+    
+    for chat_id in chat_ids:
+        try:
+            await context.bot.send_message(chat_id=chat_id, text=message)
+        except BadRequest as e:
+            if str(e) == "Chat not found":
+                logger.warning(f"Чат с ID {chat_id} не найден. Удаляем из PocketBase.")
+                remove_group_chat_id(chat_id)
+    
+    from app.bot import commands
+    await update.message.reply_text(
+        commands.get("send_news", "default send news message"),
+    )
---
+++ app/pb_models.md
@@ -0,0 +1,32 @@
+# tgbot_chats
+{
+      "id": "RECORD_ID",
+      "collectionId": "fwwayu3bobzjqzh",
+      "collectionName": "tgbot_chats",
+      "created": "2022-01-01 01:00:00.123Z",
+      "updated": "2022-01-01 23:59:59.456Z",
+      "group_id": "test"
+    },
+
+# tgbot_snippets
+{
+      "id": "RECORD_ID",
+      "collectionId": "fs1hwbt2cemscgt",
+      "collectionName": "tgbot_snippets",
+      "created": "2022-01-01 01:00:00.123Z",
+      "updated": "2022-01-01 23:59:59.456Z",
+      "is_command": true,
+      "name": "test",
+      "snippet_text": "test"
+    },
+
+# tgbot_logs
+{
+      "id": "RECORD_ID",
+      "collectionId": "bp86l9r6d95asfv",
+      "collectionName": "tgbot_logs",
+      "created": "2022-01-01 01:00:00.123Z",
+      "updated": "2022-01-01 23:59:59.456Z",
+      "group_id": "test",
+      "logs": "JSON"
+    },
